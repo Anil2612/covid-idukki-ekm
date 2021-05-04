d3783bf27ba858e390ca5a67a5ae770b2cb9e7df
Anil S Nair <anilsnair261296@gmail.com>
2021-05-04 21:24:00 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'Corona Centers';
+  vaccineDetails: any;
+  isAvailable = false;
+  isShow = true;
+  buttonLabel = 'Show All';
+  heading = 'Available Vaccine Details'
+  noItem = false;
+  availableSlot = 0;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.getData();
+  }
+
+  getData(district = 306) {
+    this.availableSlot = 0;
+    const dateData = moment().format("DD-MM-YYYY"); 
+    const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district}&date=${dateData}`;
+    this.http.get(url).subscribe((data)=>{
+      this.vaccineDetails = data;
+      this.vaccineDetails.centers.map((items: any)=>{
+        this.isAvailable = items.sessions.find((element: any) => element.available_capacity);
+        this.isAvailable ? items.isAvailable = true : items.isAvailable = false;
+        if(items.isAvailable) {
+          this.noItem = true;
+        }
+      })
+    })
+  }
+  showAll() {
+    this.isShow = !this.isShow;
+    this.isShow ? this.buttonLabel = 'Show All' : this.buttonLabel = 'Show Available';
+    this.isShow ? this.heading = 'Available Vaccine Details' : this.heading = 'Vaccine Details';
+  }
+
+  getValue(value: any) {
+    this.noItem = false;
+    value ? this.getData(307) : this.getData();
+  }
+
+
+
+
+}
